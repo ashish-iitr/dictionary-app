a7cef312e8bae01e1c1a456878a32ae4a52df915
ashish <ashish.singh@imaginea.com>
2017-05-05 17:32:16 +0530

    file structure change

--- app/dictionary/dictionary.route.js
+++ app/dictionary/dictionary.route.js
@@ -6,7 +6,7 @@ angular.module('myDictionary.route', ['ngRoute'])
  */
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/dictionary', {
-    templateUrl: 'dictionary/dictionary.html',
+    templateUrl: 'app/dictionary/dictionary.html',
     controller: 'dictionaryCtrl'
   });
 }]);
